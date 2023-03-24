ea028842fbd36639e9b6367ad469e4a0c60b99fa
thsWebstorm03 <webstorm03@gmail.com>
2023-03-24 22:36:52 +0800

    update landing

--- apps/web/src/components/PhishingWarningBanner/index.tsx
+++ apps/web/src/components/PhishingWarningBanner/index.tsx
@@ -10,10 +10,10 @@ const Container = styled(Flex)`
   padding: 12px;
   align-items: center;
   background: linear-gradient(0deg, rgba(39, 38, 44, 0.4), rgba(39, 38, 44, 0.4)),
-    linear-gradient(180deg, #8051d6 0%, #492286 100%);
+    linear-gradient(180deg, #09070c 22%, #9370db 100%);
   ${({ theme }) => theme.mediaQueries.md} {
     padding: 0px;
-    background: linear-gradient(180deg, #8051d6 0%, #492286 100%);
+    background: linear-gradient(180deg, #09070c 22%, #9370db 100%);
   }
 `
 
@@ -82,7 +82,7 @@ const PhishingWarningBanner: React.FC<React.PropsWithChildren> = () => {
         <>
           <InnerContainer>
             <img
-              src="/images/decorations/phishing-warning-bunny.webp"
+              src="/images/decorations/phishing-warning-shadow.webp"
               alt="phishing-warning"
               width="92px"
               onError={(e) => {
