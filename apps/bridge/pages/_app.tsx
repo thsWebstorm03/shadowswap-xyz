a0293399466d40aa01c5aeebbf218d9aafed2865
thsWebstorm03 <webstorm03@gmail.com>
2023-03-26 00:12:27 +0800

    update titie

--- apps/bridge/pages/_app.tsx
+++ apps/bridge/pages/_app.tsx
@@ -74,7 +74,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="🥞 PancakeSwap - A next evolution DeFi exchange on BNB Smart Chain (BSC)" />
-        <title>Bridge | PancakeSwap</title>
+        <title>Bridge | ShadowSwap</title>
       </Head>
       <NextThemeProvider>
         <StyledThemeProvider>
